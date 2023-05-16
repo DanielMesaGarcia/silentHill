4e25f6fb3f1f48a0e37ea7ad7d8478fd58c02749
Daniel <daniwlmesa12@gmail.com>
2023-05-16 16:18:28 +0100

    hosting

--- src/pages/rss/Rss.js
+++ src/pages/rss/Rss.js
@@ -6,16 +6,16 @@ function Rss() {
     <>
       <div class="backgroundN">
         <Header />
-        <div className="container">
+        <div className="containerr">
           <div className="header">
             <h1>RSS Feeds</h1>
             <p>RSS (Really Simple Syndication) is a format for delivering regularly updated web content. It allows you to easily stay informed by retrieving the latest content from your favorite websites.</p>
           </div>
-          <div className="content">
+          <div className="contentt">
             <p>Here is a list of popular RSS feeds:</p>
-            <ul class="usp">
+            <ul class="uspp">
               <a href="/assets/rss.xml" type="application/rss+xml">Subscribe to RSS Feed</a>
-              <img src="assets\img\rsss.png" class="rsimg"></img>
+              <img src="assets\img\rsss.png" class="rsimgg"></img>
             </ul>
           </div>
         </div>
